33f55cd2ead1cf14041899bca3fac76169ac8c2e
Pensk <pensk@hotmail.ca>
2018-07-10 12:12:47 +0900

    Configure webpacker

--- frontend/javascripts/calendar.jsx
+++ frontend/javascripts/calendar.jsx
@@ -1,1 +1,29 @@
-console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Calendar extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+			<div className="container">
+				<h1>Calendar</h1>
+			</div>
+		);
+	}
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const node = document.getElementById('calendar');
+  const data = JSON.parse(node.getAttribute('data'));
+  ReactDOM.render(
+    <Calendar {...data} />,
+    node
+  );
+});
+
+export default Calendar;
--- frontend/packs/calendar.js
+++ frontend/packs/calendar.js
@@ -1,1 +1,2 @@
 import 'javascripts/calendar';
+import 'stylesheets/application';
